56900683366deea660f4bae69c07d7301166be32
Aron Jones <defrex0@gmail.com>
2011-06-15 15:31:45 -0400

    notifications tweaks

--- src/js/core/notifications.js
+++ src/js/core/notifications.js
@@ -25,9 +25,37 @@ hs.nots.Notification = hs.views.View.extend({
     if (this.options.key) this.key = this.options.key;
   },
   render: function(){
-    var htm = '<div class="notification">';
-    htm += this.message;
-    htm += '</div>';
-    $('#notifications').append(htm);
-  }
+    this.getListingLink(function(listingLink){
+      var htm = '<div class="notification">';
+      htm += this.message;
+      htm += '<a href="'+listingLink+'">Go to Listing</a>';
+      htm += '</div>';
+      $('#notifications').append(htm);
+    });
+  },
+  getListingLink: function(clbk){
+    var key = this.ket.split(':'),
+        id = key[1],
+        key = key[0],
+        Model = null,
+        rel = 'listing';
+
+    switch(key){
+      case 'offer':
+        model = hs.offers.Offer;
+      break;
+      case 'message':
+        model = hs.messages.Message;
+        rel = 'offer.listing';
+      break;
+      case 'inquiry':
+        model = hs.inquiries.Inquiry;
+      break;
+    }
+
+    var model = Model.get(id);
+    model.withRel(rel, function(listing){
+      clbk.call(this, '#!/listings/'+listing._id+'/');
+    }, this);
+  },
 });
