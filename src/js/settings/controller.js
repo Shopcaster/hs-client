eda9a861011bbd77cb74f634f6607003ac90c7d4
Patrick Stein <lylepstein@gmail.com>
2011-07-05 11:55:16 -0400

    Fixed settings page loading

--- src/js/settings/controller.js
+++ src/js/settings/controller.js
@@ -12,57 +12,41 @@ hs.regController('settings', hs.Controller.extend({
     '!/settings/name': 'name'
   },
 
-  authHooked: false,
   loaded: false,
 
-  bootstrap: function(){
-    if (!this.loaded) {
-      hs.page.finish();
-      hs.page = new hs.settings.views.Settings();
-      hs.page.render();
-      this.loaded = true;
-    }
-  },
   renderView: function(){
     if (!hs.auth.isAuthenticated()) {
       hs.page.finish();
     } else {
-      this.bootstrap();
-      this.view.render();
+      hs.page.finish();
+      hs.page = new hs.settings.views.Settings();
+      hs.page.render();
+      new this.view({el: $('#settings #setting')}).render();
     }
   },
-  loadView: function(view){
-    if (!this.authHooked) {
-      hs.auth.ready(this.renderView, this);
-      this.authHooked = true;
-    }
-
-    this.view = new view({el: $('#settings #setting')});
-    this.renderView();
+  loadView: function(view, name){
+    this.view = view;
+    hs.auth.ready(function() {
+      this.renderView();
+      this.highlightLink(name);
+    }, this);
   },
   highlightLink: function(path) {
     path = '#!/settings/' + path;
     $('#settings #nav a').removeClass('selected');
     $('#settings #nav a[href="' + path + '"]').addClass('selected');
   },
-  updateAuth: function(clbk) {
-    console.log('updateauth');
-  },
 
   default: function(){
-    this.loadView(hs.settings.views.Default);
-    this.highlightLink('default');  
+    this.loadView(hs.settings.views.Default, 'default');
   },
   password: function(){
-    this.loadView(hs.settings.views.Password);
-    this.highlightLink('password');
+    this.loadView(hs.settings.views.Password, 'password');
   },
   social: function(){
-    this.loadView(hs.settings.views.Social);
-    this.highlightLink('social');
+    this.loadView(hs.settings.views.Social, 'social');
   },
   name: function(){
-    this.loadView(hs.settings.views.Name);
-    this.highlightLink('name');  
+    this.loadView(hs.settings.views.Name, 'name');
   }
 }));
